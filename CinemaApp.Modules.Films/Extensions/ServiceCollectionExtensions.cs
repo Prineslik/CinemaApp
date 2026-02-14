93dadf422f50019d2a25cd8102ad0626400cfa6e
User <prineslikilua@gmail.com>
2026-02-14 17:53:46 +0300

    Initial commit

---
+++ CinemaApp.Modules.Films/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CinemaApp.Modules.Films.Extensions
+{
+    internal class ServiceCollectionExtensions
+    {
+        pubic staic IServiceCollection AddFilmsModule(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
